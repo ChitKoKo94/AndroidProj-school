30becb516dff148bd5d0c2af66e245eceb304a98
chitko <takenheart@gmail.com>
2021-07-14 00:27:32 +0800

    download 20 images and progress bar

--- app/src/main/java/com/example/android_ca/MainActivity.java
+++ app/src/main/java/com/example/android_ca/MainActivity.java
@@ -9,18 +9,32 @@ import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String EXTENSION_PATTERN = "([^\\s]+(\\.(?i)(jpg|png))$)";
-    List<String> list = new ArrayList<>();
+    protected List<String> img_list = new ArrayList<>();
+    protected Thread bkgThread;
+
+    int[] viewId_list = {
+            R.id.imageView11,R.id.imageView12,R.id.imageView13,R.id.imageView14,R.id.imageView21,
+            R.id.imageView22,R.id.imageView23,R.id.imageView24,R.id.imageView31,R.id.imageView32,
+            R.id.imageView33,R.id.imageView34,R.id.imageView41,R.id.imageView42,R.id.imageView43,
+            R.id.imageView44,R.id.imageView51,R.id.imageView52,R.id.imageView53,R.id.imageView54
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,61 +47,80 @@ public class MainActivity extends AppCompatActivity {
             fetch.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v){
-                    int count = 0;
-                    Pattern p = Pattern.compile("<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>");
-                    Matcher m = p.matcher(URL);
-                    while (m.find()) {
-                        String srcResult = m.group(1);
-                        Pattern p2 = Pattern.compile(EXTENSION_PATTERN);
-                        Matcher img = p2.matcher(srcResult);
-                        if (img.find()) {
-                            list.add(srcResult);
-                            count++;
+                    img_list.clear();
+                    if (bkgThread != null) {
+                        bkgThread.interrupt();
+                        for(int p=0; p<20; p++) {
+                            ImageView imageView = findViewById(viewId_list[p]);
+                            imageView.setImageDrawable(getDrawable(R.drawable.sample));
                         }
-                        if (count == 20) break;
                     }
-                    startDownloadImage(list);
+                    bkgThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Document document = Jsoup.connect(URL).get();
+                                Elements tags = document.getElementsByTag("img");
+                                String src;
+                                for(int i=0; i<tags.size(); i++){
+                                    src = tags.get(i).attr("src");
+                                    if (src.contains(".png") || src.contains(".jpg")){
+                                        img_list.add(src);
+                                    }
+                                    if (img_list.size() == 20) break;
+                                }
+                            } catch (IOException e){
+                                e.printStackTrace();
+                            }
+                            startDownloadImage(img_list);
+                        }
+                    });
+                    bkgThread.start();
                 }
             });
         }
     }
 
-    protected void startDownloadImage(List<String> imgURLs){
+    protected void startDownloadImage(List<String> imglist){
         File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         List<File> destFile_list = new ArrayList<>();
+        TextView status = findViewById(R.id.protext);
+        ProgressBar probar = findViewById(R.id.probar);
 
         for(int i=0; i<20; i++){
-            String destFilename = UUID.randomUUID().toString() + imgURLs.get(i).lastIndexOf(".") + i;
+            String destFilename = UUID.randomUUID().toString() + imglist.get(i).lastIndexOf(".") + i;
             File destFile = new File(dir, destFilename);
             destFile_list.add(destFile);
         }
 
-        int[] viewId_list = {
-                R.id.imageView11,R.id.imageView12,R.id.imageView13,R.id.imageView14,R.id.imageView21,
-                R.id.imageView22,R.id.imageView23,R.id.imageView24,R.id.imageView31,R.id.imageView32,
-                R.id.imageView33,R.id.imageView34,R.id.imageView41,R.id.imageView42,R.id.imageView43,
-                R.id.imageView44,R.id.imageView51,R.id.imageView52,R.id.imageView53,R.id.imageView54
-        };
+        ImageDownloader imgDL = new ImageDownloader();
+        for (int k = 0; k < 20; k++) {
+            if (imgDL.downloadImages(imglist.get(k), destFile_list.get(k))) {
+                int finalK = k;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Bitmap bitmap = BitmapFactory.decodeFile(destFile_list.get(finalK).getAbsolutePath());
+                        ImageView imageView = findViewById(viewId_list[finalK]);
+                        imageView.setImageBitmap(bitmap);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ImageDownloader imgDL = new ImageDownloader();
-                for(int k=0; k<20; k++){
-                    if (imgDL.downloadImages(imgURLs.get(k), destFile_list.get(k))){
-                        int finalK = k;
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Bitmap bitmap = BitmapFactory.decodeFile(destFile_list.get(finalK).getAbsolutePath());
-                                ImageView imageView = findViewById(viewId_list[finalK]);
-                                imageView.setImageBitmap(bitmap);
-                            }
-                        });
+                        status.setVisibility(View.VISIBLE);
+                        status.setText(finalK+1+"/20 images downloaded...");
+                        probar.setVisibility(View.VISIBLE);
+                        probar.setProgress((finalK+1)*10);
                     }
-                }
-
+                });
             }
-        }).start();
+        }
     }
+
+//    protected void progressbar(int num){
+//        TextView status = findViewById(R.id.protext);
+//        ProgressBar probar = findViewById(R.id.probar);
+//
+//        status.setVisibility(View.VISIBLE);
+//        status.setText(num+"/20 images downloaded...");
+//        probar.setVisibility(View.VISIBLE);
+//        probar.setProgress(num);
+//    }
 }
