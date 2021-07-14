ea66c930a2f6d8744571dd7bbb173cf578582c8a
vidya-eln <e0696867@u.nus.edu>
2021-07-14 17:36:47 +0800

    commit 2

--- app/src/main/java/com/example/android_ca/MainActivity.java
+++ app/src/main/java/com/example/android_ca/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.android_ca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -23,21 +18,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
-    //vidya
-
-    private static final String EXTENSION_PATTERN = "([^\\s]+(\\.(?i)(jpg|png))$)";
-    List<String> list = new ArrayList<>();
-
-    ArrayList<ImageView> selectedImgs = new ArrayList<ImageView>();
 
     protected List<String> img_list = new ArrayList<>();
     protected Thread bkgThread;
@@ -54,20 +41,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button b1=findViewById(R.id.tag_flower);
+        Button b2=findViewById(R.id.tag_love);
+        Button b3=findViewById(R.id.tag_biz);
+        Button b4=findViewById(R.id.tag_travel);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://stocksnap.io//search/flower";
+                EditText req_url=findViewById(R.id.url);
+                req_url.setText(url);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://stocksnap.io//search/love";
+                EditText req_url=findViewById(R.id.url);
+                req_url.setText(url);
+
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://stocksnap.io//search/business";
+                EditText req_url=findViewById(R.id.url);
+                req_url.setText(url);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://stocksnap.io//search/travel";
+                EditText req_url=findViewById(R.id.url);
+                req_url.setText(url);
+            }
+        });
+
+
+
         Button fetch = findViewById(R.id.fetch);
         if (fetch != null){
             fetch.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v){
+
                     img_list.clear();
                     EditText req_url = findViewById(R.id.url);
                     String url = req_url.getText().toString();
                     if (bkgThread != null) {
                         bkgThread.interrupt();
+
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 for(int p=0; p<20; p++) {
+
+
                                     ImageView imageView = findViewById(viewId_list[p]);
                                     imageView.setImageDrawable(getDrawable(R.drawable.sample));
                                 }
@@ -83,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
                                 Elements tags = document.getElementsByTag("img");
                                 String src;
                                 for(int i=0; i<tags.size(); i++){
+
+
                                     src = tags.get(i).attr("src");
                                     if (src.contains(".png") || src.contains(".jpg")){
                                         img_list.add(src);
@@ -99,32 +132,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-
-
-        Drawable border = getDrawable( R.drawable.border);
-        for(int id: viewId_list)
-        {
-            ImageView img = (ImageView)findViewById(id);
-            img.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (selectedImgs.contains(img)) {
-                        selectedImgs.remove(img);
-                        img.setBackgroundResource(0);
-                    } else {
-                        selectedImgs.add(img);
-                        img.setBackground(border);
-                    }
-
-                    if (selectedImgs.size() == 6) {
-                        saveImgs();
-                        Intent intent = new Intent(MainActivity.this, GameTest.class);
-                        startActivity(intent);
-                    }
-                }
-            });
-        }
-
     }
 
     protected void startDownloadImage(List<String> imglist){
@@ -141,11 +148,16 @@ public class MainActivity extends AppCompatActivity {
 
         ImageDownloader imgDL = new ImageDownloader();
         for (int k = 0; k < 20; k++) {
+
+
             if (imgDL.downloadImages(imglist.get(k), destFile_list.get(k))) {
                 int finalK = k;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        if(bkgThread.isInterrupted())
+                            return;
+
                         Bitmap bitmap = BitmapFactory.decodeFile(destFile_list.get(finalK).getAbsolutePath());
                         ImageView imageView = findViewById(viewId_list[finalK]);
                         imageView.setImageBitmap(bitmap);
@@ -157,22 +169,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-        }
-    }
-
-    private void saveImgs(){
-        for (int i = 0; i<6; i++){
-            String imageName ="image" +i;
-            FileOutputStream fileOutputStream;
-            try {
-                fileOutputStream = getApplicationContext().openFileOutput(imageName, Context.MODE_PRIVATE);
-                Bitmap bitmap = ((BitmapDrawable)selectedImgs.get(i).getDrawable()).getBitmap();
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
-                fileOutputStream.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
         }
     }
 
